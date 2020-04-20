d8969de08b59a535c5523ab22a3cad3a49e95d51
Gyeong <chung3286@naver.com>
2020-04-20 16:31:26 +0900

    v2.0

--- Gugudan/src/Gugudan.java
+++ Gugudan/src/Gugudan.java
@@ -5,7 +5,9 @@ public class Gugudan {
         Scanner scan = new Scanner(System.in);
         System.out.printf("Put number : ");
         int n = scan.nextInt();
-        if(n == 0) {
+        if(n<0) {
+        	System.out.printf("¡Ø Cannot Operate ¡Ø");
+        }else if(n == 0) {
             System.out.println("------Result------");
         	for(int i=1; i<10; i++) {
         		for(int j=2; j<10; j++) {
